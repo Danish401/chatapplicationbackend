9bf1e0ef094001c58aecfcf795c69583cca0b3cc
Danish401 <125354722+Danish401@users.noreply.github.com>
2025-02-23 22:52:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: ['http://localhost:3000', 'http://localhost:3001'],
+    origin: ['https://chatapplication29.netlify.app', 'http://localhost:3001'],
     // credentials: true,
   },
 });
